4f94d299f49d62f3adbe9912a138be64dfeecd90
lipun <lenkapradyumna1404@gmail.com>
2026-07-13 19:42:54 +0530

    implement echo

--- app/main.js
+++ app/main.js
@@ -14,6 +14,9 @@ rl.on("line", (command) => {
     rl.close();
     return;
   }
+  if(command.startsWith("echo ")) {
+    console.log(command.slice(5));    
+  }
   console.log(`${command}: command not found`);
   rl.prompt();
 })
