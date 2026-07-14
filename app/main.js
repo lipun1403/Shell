fc63d766dfde8494f3a0c25421e8d2c50b65bec3
lipun <lenkapradyumna1404@gmail.com>
2026-07-14 06:12:17 +0530

    extend type to search executable file

--- app/main.js
+++ app/main.js
@@ -1,11 +1,39 @@
-const readline = require("readline");
+import { createInterface } from "readline";
+import { delimiter, resolve } from "path";
+import { accessSync, constants, statSync } from "fs";
+
 
 // default interface with '$' sign
-const rl = readline.createInterface({
+const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: "$ ",
 });
+
+function isShellBuiltin(command) {
+  const builtins = ["echo", "type", "exit"];
+  return builtins.includes(command);
+}
+
+function findExecutable(command) {
+  const path = process.env.PATH || "";
+
+  for(const directory of path.split(delimiter)) {
+    if(!directory) continue;
+    const fullPath = resolve(directory, command);
+    try {
+      const stats = statSync(fullPath);
+      if(stats.isFile()) {
+        accessSync(fullPath, constants.X_OK);
+        return fullPath;
+      }
+    }
+    catch(e) {
+      
+    }
+  }
+  return null;
+}
 
 rl.prompt();
 
@@ -19,15 +47,21 @@ rl.on("line", (command) => {
   }
   else if(command.startsWith("type ")) {
     const cmd = command.slice(5);
-    if(cmd == "type" || cmd == "echo" || cmd == "exit") {
+    if(isShellBuiltin(cmd)) {
       console.log(`${cmd} is a shell builtin`);
     }
     else {
-      console.log(`${cmd}: not found`);      
+      const executablePath = findExecutable(cmd);
+      if(executablePath) {
+        console.log(`${cmd} is ${executablePath}`);
+      }
+      else {
+        console.log(`${cmd}: command not found`);
+      }
     }
   }
   else {
     console.log(`${command}: command not found`);
   }
-    rl.prompt();
+  rl.prompt();
 })
